6748e4a8e16ca1691cc8192b7800265fe613c89e
yoosuf14 <62879093+yoosuf14@users.noreply.github.com>
2022-10-07 21:41:44 +0530

    created survivor entity and tested

--- src/main/java/com/zssn/zssn/controller/ApiController.java
+++ src/main/java/com/zssn/zssn/controller/ApiController.java
@@ -1,4 +1,13 @@
 package com.zssn.zssn.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ApiController {
+    @GetMapping("/")
+    public String getPage(){
+        return "<h1>Welcome to Zombie Survival Social Network!<h1>";
+    }
+
 }
---
+++ src/main/java/com/zssn/zssn/models/survivor.java
@@ -0,0 +1,80 @@
+package com.zssn.zssn.models;
+
+import javax.persistence.*;
+
+@Entity
+public class survivor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column
+    private String name;
+    @Column
+    private int age;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public double getLocactionX() {
+        return locactionX;
+    }
+
+    public void setLocactionX(double locactionX) {
+        this.locactionX = locactionX;
+    }
+
+    public double getLocactionY() {
+        return locactionY;
+    }
+
+    public void setLocactionY(double locactionY) {
+        this.locactionY = locactionY;
+    }
+
+    public boolean isInfected() {
+        return infected;
+    }
+
+    public void setInfected(boolean infected) {
+        this.infected = infected;
+    }
+
+    @Column
+    private String gender;
+    @Column
+    private double locactionX;
+    @Column
+    private double locactionY;
+    @Column
+    private boolean infected;
+
+}
