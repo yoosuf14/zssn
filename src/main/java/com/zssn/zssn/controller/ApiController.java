44b36fd611eac9fa6b1129219d2390507cb4b7fd
yoosuf14 <62879093+yoosuf14@users.noreply.github.com>
2022-10-07 22:02:10 +0530

    autowired Repos in apiController

--- src/main/java/com/zssn/zssn/controller/ApiController.java
+++ src/main/java/com/zssn/zssn/controller/ApiController.java
@@ -1,10 +1,17 @@
 package com.zssn.zssn.controller;
 
+import com.zssn.zssn.Repo.InventoryRepo;
+import com.zssn.zssn.Repo.SurvivorRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApiController {
+    @Autowired
+    private SurvivorRepo survivorRepo;
+    @Autowired
+    private InventoryRepo inventoryRepo;
     @GetMapping("/")
     public String getPage(){
         return "<h1>Welcome to Zombie Survival Social Network!<h1>";
