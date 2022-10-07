fa8737f75c2b4a37cdb29cfbcc147906b361edfb
yoosuf14 <62879093+yoosuf14@users.noreply.github.com>
2022-10-08 01:09:13 +0530

    infected percentage endpoint created
    Created upto infected percentage. Requires some formatting.

--- src/main/java/com/zssn/zssn/Repo/SurvivorRepo.java
+++ src/main/java/com/zssn/zssn/Repo/SurvivorRepo.java
@@ -4,4 +4,6 @@ import com.zssn.zssn.models.Survivor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SurvivorRepo extends JpaRepository<Survivor, Long> {
+    Survivor findByGender(String gender);
+    long countByInfected(boolean isInfected);
 }
--- src/main/java/com/zssn/zssn/controller/ApiController.java
+++ src/main/java/com/zssn/zssn/controller/ApiController.java
@@ -5,11 +5,9 @@ import com.zssn.zssn.Repo.SurvivorRepo;
 import com.zssn.zssn.models.Inventory;
 import com.zssn.zssn.models.Survivor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Formatter;
 import java.util.List;
 
 @RestController
@@ -32,6 +30,16 @@ public class ApiController {
         survivorRepo.save(survivor);
         return "Successfully registered survivor!";
     }
+    @PutMapping(value="/updateSurvivor/{id}")
+    public String saveSurvivor(long id,@RequestBody Survivor survivor){
+        Survivor updatedSurvivor = survivorRepo.findById(id).get();
+        updatedSurvivor.setName(survivor.getName());
+        updatedSurvivor.setAge(survivor.getAge());
+        updatedSurvivor.setGender(survivor.getGender());
+        updatedSurvivor.setInfected(survivor.isInfected());
+        return "Successfully updated survivor!";
+    }
+
 
     @GetMapping(value="/allInventory")
     public List<Inventory> getInventory(){
@@ -42,7 +50,24 @@ public class ApiController {
         inventoryRepo.save(inventory);
         return "Successfully created inventory!";
     }
+    @GetMapping(value="/totalSurvivors")
+    public long countSurvivors(){
+        long count = survivorRepo.count();
+        return count;
+    }
+    @GetMapping(value="/infectedSurvivors")
+    public long countInfectedSurvivors(){
+        long count = survivorRepo.countByInfected(true);//table stores in bit format
+        return count;
+    }
+    @GetMapping("/infectedPercentage")
+    public String getInfectedPercentage(){
+        double infectedRatio= ((double)countInfectedSurvivors()/(double)countSurvivors())*100;
 
+        Formatter formatter = new Formatter();
+        formatter.format("%.2f", infectedRatio);
+        return infectedRatio+"%";
+    }
 
 
 }
